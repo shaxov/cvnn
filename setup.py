2b2ae2650e14a0c8fa989ce672337a3940b65831
Maksym Shpakovych <maksym.shpakovych@gmail.com>
2020-09-07 13:45:10 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cvnn",
+    version="0.0.1",
+    author="Maksym Shpakovych",
+    author_email="maksym.shpakovych@gmail.com",
+    description="Complex-valued blocks for neural network building.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shaxov/cvnn",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
